1f2777d68510506100e9e70d8cd8abb0d9268174
timmiedepimmie <tim.flikweert@hotmail.com>
2021-02-10 17:06:00 +0100

    aantekeningen

---
+++ aantekeningen/week2.java
@@ -0,0 +1,53 @@
+/*
+	void start() {
+		System.out.println("Dag Kijker! Voer je naam in");
+		Scanner sc = new Scanner(System.in);
+		String naam = sc.nextLine();
+		System.out.println("Welkom " + naam + ". Laten we beginnen.");
+
+		Hond hondje = new Hond();
+		hondje.ademen();
+		hondje.blaffen();
+
+		int[] getallen = { 5, 7, 8 };
+		String[] woorden = { "woord1", "woord2" };
+		Hond[] honden = { new Hond(), new Hond() };
+		int[][] tweeDarray = { { 3, 4, 5 }, { 5, 7, 8 }, { 2, 6, 9 } };
+		System.out.println(tweeDarray[1][2]); // geeft 8
+
+	}
+}
+
+class Dier {
+	int leeftijd;
+
+	void ademen() {
+		System.out.println("Het dier ademt nu");
+	}
+}
+
+class Hond extends Dier {
+	String hondenras;
+
+	void blaffen() {
+		System.out.println("Woef!");
+	}
+}
+*/
+	
+/*
+ * class Nederlander{ static final int aantalogen??; // oprichtingsdatum,
+ * eerstekoning, volkslied, WOII, NL, static int belasting; //Stemrechtregels,
+ * final gebdatum; woonplaats }
+ * 
+ * 
+ * class Mens{ static final String brandstofSpier = "O2"; final String
+ * vingerafdruk = ""; static int avondkloktijdstip; String kleurhaar; }
+ * 
+ * class Auto{ static final String doel??; final typemotor; static
+ * rdwkeuringRegels;??? bekleding; }
+ * 
+ * 
+ * class Plant{ static final boolean heeftCelwand = true; final zaaidatum;
+ * static boolean naamNaturalis; String naamNaturalis; int aantalBladeren; }
+ */
